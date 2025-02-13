ae7e126880141112799a380326246f471bc15dd3
H.Morimoto <hmorimoto704@gmail.com>
2025-02-14 08:41:51 +0900

    SSCUtility

---
+++ SSCUtility/CriptoUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSCUtility
+{
+    public static class CriptoUtility
+    {
+        public static byte[] GetHash(int[] src)
+        {
+            List<byte> bytes = new();
+            src.Select(e => BitConverter.GetBytes(e))
+                .ToList()
+                .ForEach(e => bytes.AddRange(e));
+            return MD5.HashData([.. bytes]);
+        }
+    }
+}
